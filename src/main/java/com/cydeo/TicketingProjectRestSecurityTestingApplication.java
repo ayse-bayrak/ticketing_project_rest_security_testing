7686402a79c8a037075bb1c8b49a5f7b6d1138ed
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-17 19:22:25 +0300

    Testing Part is started

--- src/main/java/com/cydeo/TicketingProjectRestSecurityTestingApplication.java
+++ src/main/java/com/cydeo/TicketingProjectRestSecurityTestingApplication.java
@@ -1,13 +1,21 @@
-package com.cydeo.ticketing_project_rest_security_testing;
+package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectRestSecurityTestingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectRestSecurityTestingApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+
+        return new ModelMapper();
     }
 
 }
--- src/test/java/com/cydeo/TicketingProjectRestSecurityTestingApplicationTests.java
+++ src/test/java/com/cydeo/TicketingProjectRestSecurityTestingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cydeo.ticketing_project_rest_security_testing;
+package com.cydeo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
